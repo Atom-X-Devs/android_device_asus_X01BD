43f7a7bda94eda3e1848fb59660c545159513c8c
Kunmun <kunmun.devroms@gmail.com>
2021-03-08 07:21:44 +0000

    X01BD: CitricOS Official
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The KangOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/citric_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    citric_X01BD-eng \
+    citric_X01BD-user \
+    citric_X01BD-userdebug
